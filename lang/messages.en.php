02432f56f5e841a05ffa4256939a2ddbcb6e7418
GitLab <git@gitlab.s.intranet>
2016-12-19 14:25:21 +0300

    tao version 0.1.14

--- lang/messages.en.php
+++ lang/messages.en.php
@@ -1,4 +1,9 @@
 <?php return array(
     'ajax_more_button' => 'More',
     'composite_loading' => 'Loading...',
+    'no_results' => 'No results',
+    'search_query' => 'Search query: <strong>%s</strong>',
+    'search' => 'Search',
+    'find' => 'Find',
+    'search_count' => 'Found: <strong>%s</strong>',
 );
