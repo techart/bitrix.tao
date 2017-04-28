6a6498e25609afeeef8f2663a730f1f3f137c6a0
TechartWebDev <webdev@techart.ru>
2017-04-28 11:09:07 +0300

    Version 0.1.29

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,9 +1,11 @@
 <?php
 $uri = $_SERVER['REQUEST_URI'];
+$clean_uri = $uri;
 
 $p = strpos($uri, '?');
 if ($p > 0) {
     $uri = substr($uri, 0, $p);
+    $clean_uri = $uri;
 }
 
 if ($uri[strlen($uri) - 1] == '/') {
@@ -27,6 +29,7 @@ if (is_file($_tao_path)) {
     $_SERVER['SCRIPT_NAME'] = $_SERVER['DOCUMENT_URI'] = $_SERVER['PHP_SELF'] = $uri;
     chdir(dirname($_tao_path));
     unset($uri);
+    unset($clean_uri);
     unset($p);
     unset($m);
     include($_tao_path);
@@ -79,6 +82,22 @@ if (is_string($content)) {
     die;
 }
 
+if ($clean_uri!='' && substr($clean_uri, count($clean_uri)-1)!='/') {
+    $r = \TAO::getOption('redirect_if_no_slash');
+    $r = is_null($r)? true : $r;
+    if ($r) {
+        $clean_uri .= '/';
+        $q = trim($_SERVER['QUERY_STRING']);
+        if ($q!='') {
+            $clean_uri .= '?' . $q;
+        }
+        header("HTTP/1.1 301 Moved Permanently"); 
+        header("Location: {$clean_uri}");
+    }
+    unset($r);
+}
+
+
 $nativeScript = '/bitrix/urlrewrite.php';
 $_SERVER['SCRIPT_FILENAME'] = $_SERVER['DOCUMENT_ROOT'] . $nativeScript;
 $_SERVER['SCRIPT_NAME'] = $_SERVER['DOCUMENT_URI'] = $_SERVER['PHP_SELF'] = $nativeScript;
@@ -87,5 +106,7 @@ chdir($_SERVER['DOCUMENT_ROOT'] . '/bitrix');
 unset($script);
 unset($nativeScript);
 unset($content);
+unset($clean_uri);
+unset($uri);
 include($_SERVER['SCRIPT_FILENAME']);
 
