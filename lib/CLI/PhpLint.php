6376df226dabad04e09ab0ec99e97af352c133e5
Git Techart <git@tchart.ru>
2023-03-02 06:49:13 +0000

    Version 1.19.1

--- lib/CLI/PhpLint.php
+++ lib/CLI/PhpLint.php
@@ -10,7 +10,7 @@ class PhpLint extends CLI
 	{
 		echo 'Php-lint start...';
 		$path = exec('pwd');
-		exec("cd $path/local/php-tooling && php ./vendor/bin/phpcs ../ --colors", $output);
+		exec("cd $path/local/php-tooling && php ./vendor/bin/phpcs ../../ --colors", $output);
 		echo implode("\n", $output);
 
 	}
